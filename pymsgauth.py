81d1c56f32f4c91d04fcb6c956118a9f70fe192a
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-07-24 10:48:47 +0200

    Set version to indicate patchiness.

--- pymsgauth.py
+++ pymsgauth.py
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 '''
 
-__version__ = '2.1.0'
+__version__ = '2.1.0-filter3-20180724'
 __author__ = 'Charles Cazabon <software @ discworld.dyndns.org>'
 
 
