c20e921dcb34205db3c89d018fa089cd518a6f65
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-08-02 16:15:48 +0200

    Close open files and fix other Python 3 warnings.

--- test_rfc822_message.py
+++ test_rfc822_message.py
@@ -4,6 +4,7 @@ import unittest
 
 import codecs
 import email
+import os
 import sys
 
 
@@ -48,17 +49,24 @@ class RFC822Message:
         return addrlist
 
     def rewindbody(self):
-        if self.fp != sys.stdin:
-            self.init_fp(self.fp)
+        self.init_fp(self.fp)
 
 
 class TestRFC822Message(unittest.TestCase):
     def message3(self, which):
-        f = open('sample_message_' + which + '.txt', 'r')
-        return RFC822Message(f)
+        self.f = open('sample_message_' + which + '.txt', 'r')
+        self.message = RFC822Message(self.f)
+
+    def setUp(self):
+        self.f = None
+        self.message = None
+
+    def tearDown(self):
+        if self.f:
+            self.f.close()
 
     def test_headers_preserves_order(self):
-        message = self.message3('in')
+        self.message3('in')
 
         expected_headers_in_order = """From: "Amitai Schleier" <schmonz@schmonz.com>
 Cc: archive@schmonz.com (Archive Schmonz)
@@ -71,23 +79,24 @@ MIME-Version: 1.0
 Content-Type: text/plain; charset=utf-8; format=flowed
 Content-Transfer-Encoding: 8bit
 """
-        actual_headers = message.headers
+        actual_headers = self.message.headers
 
         self.assertIsInstance(actual_headers, list)
-        self.assertEqual(expected_headers_in_order, ''.join(message.headers))
+        self.assertEqual(expected_headers_in_order, ''.join(self.message.headers))
 
     def test_readline_fp_reads_line(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        line1 = message.fp.readline()
-        line2 = message.fp.readline()
-        line3 = message.fp.readline()
+        line1 = self.message.fp.readline()
+        line2 = self.message.fp.readline()
+        line3 = self.message.fp.readline()
 
         self.assertTrue(line1.startswith('This patch '))
         self.assertTrue(line2.startswith('(http://pyropus.ca'))
         self.assertEqual(u"\n", line3)
 
     def test_rewindbody_on_stdin_does_not_rewind(self):
+        sys.stdin = os.fdopen(sys.stdin.fileno(), 'rb', 0)
         sys.stdin = codecs.getreader('utf-8')(sys.stdin)
         message = RFC822Message(sys.stdin)
 
@@ -100,88 +109,88 @@ Content-Transfer-Encoding: 8bit
         self.assertFalse(current_line.startswith('This patch '))
 
     def test_rewindbody_on_file_rewinds(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        message.fp.readline()
-        message.fp.readline()
-        message.fp.readline()
-        message.rewindbody()
-        current_line = message.fp.readline()
+        self.message.fp.readline()
+        self.message.fp.readline()
+        self.message.fp.readline()
+        self.message.rewindbody()
+        current_line = self.message.fp.readline()
 
         self.assertTrue(current_line.startswith('This patch '))
 
     def test_getaddr_on_missing_header_gives_none(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        blorf_name, blorf_addr = message.getaddr('blorf')
+        blorf_name, blorf_addr = self.message.getaddr('blorf')
 
         self.assertEqual(None, blorf_name)
         self.assertEqual(None, blorf_addr)
 
     def test_getaddr_on_non_address_header_still_parses(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        mailer_name, mailer_addr = message.getaddr('x-mailer')
+        mailer_name, mailer_addr = self.message.getaddr('x-mailer')
 
         self.assertEqual('1.11.3r5509', mailer_name)
         self.assertEqual('MailMate', mailer_addr)
 
-        mimeversion_name, mimeversion_addr = message.getaddr('mime-version')
+        mimeversion_name, mimeversion_addr = self.message.getaddr('mime-version')
 
         self.assertEqual('', mimeversion_name)
         self.assertEqual('1.0', mimeversion_addr)
 
     def test_getaddr_on_sensible_header_gives_values(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        from_name, from_addr = message.getaddr('from')
+        from_name, from_addr = self.message.getaddr('from')
 
         self.assertEqual('Amitai Schleier', from_name)
         self.assertEqual('schmonz@schmonz.com', from_addr)
 
     def test_getaddr_on_also_sensible_header_gives_values(self):
-        message = self.message3('in')
+        self.message3('in')
 
-        from_name, from_addr = message.getaddr('cc')
+        from_name, from_addr = self.message.getaddr('cc')
 
         self.assertEqual('Archive Schmonz', from_name)
         self.assertEqual('archive@schmonz.com', from_addr)
 
     def test_getheader_on_missing_header_gives_empty(self):
-        message = self.message3('out')
+        self.message3('out')
         field = 'X-totally-invented-for-this-test'
 
-        header = message.getheader(field, '')
+        header = self.message.getheader(field, '')
 
         self.assertEqual(0, len(header))
         self.assertRegexpMatches(header, r'^$')
 
     def test_getheader_on_present_header_gives_value(self):
-        message = self.message3('out')
+        self.message3('out')
         field = 'X-pymsgauth-token'
 
-        header = message.getheader(field, '')
+        header = self.message.getheader(field, '')
 
         self.assertEqual(40, len(header))
         self.assertRegexpMatches(header, r'[:xdigit:]+')
 
     def test_getaddrlist_makes_tuples(self):
-        message = self.message3('in')
+        self.message3('in')
 
         recipients = []
         self.assertEqual(0, len(recipients))
 
-        recipients.extend(message.getaddrlist('to'))
+        recipients.extend(self.message.getaddrlist('to'))
         self.assertEqual(1, len(recipients))
 
         name, addr = recipients[0]
         self.assertEqual('', name)
         self.assertEqual('qmail@list.cr.yp.to', addr)
 
-        recipients.extend(message.getaddrlist('blorf'))
+        recipients.extend(self.message.getaddrlist('blorf'))
         self.assertEqual(1, len(recipients))
 
-        recipients.extend(message.getaddrlist('from'))
+        recipients.extend(self.message.getaddrlist('from'))
         self.assertEqual(2, len(recipients))
 
         name, addr = recipients[1]
